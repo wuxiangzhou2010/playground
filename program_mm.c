985d28ba33e0d699703414d695e2c97833738268
xihajuan2010@gmail.com <xihajuan2010@gmail.com>
2017-09-20 16:49:19 +0800

    add comment

--- program_mm.c
+++ program_mm.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-int temp1;
+int temp1;//bss
 int main(int argc, char* argv[])
 {
-    int test[128];
-    int temp[128]={0};
-    void *p;
-    p = malloc(128*sizeof(int)) ;  
+    int test[128];//stack,uninitialized
+    int temp[128]={0};//stack, initialized
+    void *p;//stack
+    p = malloc(128*sizeof(int)) ;//heap  
     free(p);
     return 0;
 }
